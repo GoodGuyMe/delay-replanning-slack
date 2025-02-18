a11db9e6314985ebbe845af0d815abdff680749e
ekemmeren <e.a.kemmeren@student.tudelft.nl>
2025-02-18 11:10:40 +0100

    Better showing of headway in a distance time graph

--- generation/safe_interval_graph.py
+++ generation/safe_interval_graph.py
@@ -1,4 +1,20 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+def replaceAB(node, intervals):
+    if "A" in node:
+        node = node.replace("AR", "B")
+        node = node.replace("AL", "B")
+        node = node.replace("A", "B")
+        if node + "R" in intervals:
+            return node + "L"
+        return node
+    node = node.replace("BR", "A")
+    node = node.replace("BL", "A")
+    node = node.replace("B", "A")
+    if node + "R" in intervals:
+        return node + "L"
+    return node
 
 def plot_safe_node_intervals(safe_node_intervals, moves_per_agent=None):
     if moves_per_agent is None:
@@ -38,31 +54,57 @@ def plot_safe_node_intervals(safe_node_intervals, moves_per_agent=None):
         plt.title(f"Agent {agent_id}")
         plt.show()
 
-def plot_unsafe_node_intervals(unsafe_node_intervals, moves_per_agent):
-    for agent_id, movements in moves_per_agent.items():
-        node_map = dict()
-        y = 0
-        for movement in movements:
-            for edge in movement:
-                node = edge.from_node.name
-                if node not in node_map:
-                    y += edge.length
-                    node_map[node] = y
-                node = edge.to_node.name
-                if node not in node_map:
-                    y += edge.length
-                    node_map[node] = y
-                plt.plot([edge.start_time, edge.depart_time], [node_map[edge.from_node.name], node_map[edge.to_node.name]], color='green', linestyle='--')
         for node, y in node_map.items():
-            for start, stop, duration in unsafe_node_intervals[node]:
-                plt.plot([start, start + duration], [y, y], color='green')
-                plt.plot([start + duration, stop], [y, y], color='red')
+            for start, stop in safe_node_intervals[replaceAB(node, safe_node_intervals)]:
+                plt.plot([start, stop], [y, y], color='green')
 
         y_axis = list(node_map.values())
-        ytics = list(node_map.keys())
+        ytics = [replaceAB(node,safe_node_intervals) for node in node_map.keys()]
 
         # plt.xlim([0, 4000])
 
         plt.yticks(y_axis, ytics)
-        plt.title(f"Agent {agent_id}")
+        plt.title(f"Agent {agent_id} opposite")
         plt.show()
+
+def plot_unsafe_node_intervals(unsafe_node_intervals, moves_per_agent, fixed_block=False):
+    node_map = dict()
+    y = 0
+    ax = plt.gca()
+    max_time = 0
+    for agent_id, movements in moves_per_agent.items():
+        for movement in movements:
+            for edge in movement:
+                node = edge.from_node.name
+                if node not in node_map:
+                    node_map[node] = (y, edge)
+                    y += edge.length
+                # node = edge.to_node.name
+                # if node not in node_map:
+                #     y += edge.length
+                #     node_map[node] = (y, edge.length)
+                y_cur, old_len = node_map[node]
+                plt.plot([y_cur, y_cur + edge.length], [edge.start_time, edge.depart_time], color='black', linestyle='--')
+                max_time = max(max_time, edge.depart_time)
+        for node, (y, edge) in node_map.items():
+            for start, stop, duration in unsafe_node_intervals[node]:
+                if fixed_block:
+                    occupied = patches.Rectangle((y, start), edge.length, duration, linewidth=1, edgecolor='red', facecolor='none')
+                    ax.add_patch(occupied)
+                else:
+                    plt.plot([y, y+edge.length], [start + 180, start + duration + 180], color='red', linestyle='-')
+                # ax.add_patch(headway)
+                # plt.plot([start, start + duration], [y, y], color='green')
+                # plt.plot([start + duration, stop], [y, y], color='red')
+
+    plt.ylabel(f"Time (s)")
+    plt.xlabel(f"Distance")
+
+    x_axis = [n for (n, _) in node_map.values()]
+    xtics = list(node_map.keys())
+
+    # plt.ylim([0, 4000])
+
+    plt.xticks(x_axis, xtics)
+    plt.title(f"Agent {agent_id}")
+    plt.show()
