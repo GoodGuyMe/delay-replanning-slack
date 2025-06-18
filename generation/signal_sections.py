6edec17ac4aed62604449b92ed17a4a743d81a4e
ekemmeren <e.a.kemmeren@student.tudelft.nl>
2025-06-18 23:05:16 +0200

    Fix routing graph creation

--- generation/signal_sections.py
+++ generation/signal_sections.py
@@ -7,14 +7,13 @@ def convertMovesToBlock(moves_per_agent, g: TrackGraph) -> dict[int, list[BlockE
     for agent in moves_per_agent:
         block_route = []
         for movements in moves_per_agent[agent]:
-            blocks = None
+            blocks = {block for block in movements[0].to_node.blocks if isinstance(block, Edge)}
             for move in movements:
-                if move.from_node in signal_tracks:
-                    if blocks:
-                        if len(blocks) != 1:
-                            raise ValueError(f"Should really only be one, {blocks}")
-                        block_route.append(list(blocks)[0])
-                    blocks = {block for block in move.to_node.blocks if isinstance(block, Edge)}
                 blocks = blocks & {block for block in move.to_node.blocks if isinstance(block, Edge) and block.direction == move.direction}
+                if move.to_node in signal_tracks:
+                    if len(blocks) != 1:
+                        raise ValueError(f"Should really only be one, {blocks}")
+                    block_route.append(list(blocks)[0])
+                    blocks = {block for block in move.to_node.blocks if isinstance(block, Edge)}
         block_routes[agent] = [block_route]
     return block_routes
